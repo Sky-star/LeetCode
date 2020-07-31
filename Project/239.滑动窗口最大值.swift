57aef988db21014251b08496def0415ae1582739
白泉 <417941258@qq.com>
2020-07-31 17:35:03 +0800

    滑动窗口最大值

--- Project/239.滑动窗口最大值.swift
+++ Project/239.滑动窗口最大值.swift
@@ -7,7 +7,14 @@
 // @lc code=start
 class Solution {
     func maxSlidingWindow(_ nums: [Int], _ k: Int) -> [Int] {
-        
+        var maxInt = [Int]()
+        var maxRes = Int.min
+        var stack = nums[0..<k]
+        for val in nums[k..<nums.count] {
+            
+        }
+
+        return maxInt
     }
 }
 // @lc code=end
