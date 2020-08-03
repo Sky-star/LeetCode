7967d0b8978723e9e4c6ffa695480d1ccfcd1fef
白泉 <417941258@qq.com>
2020-08-03 17:21:50 +0800

    no message

--- Project/239.滑动窗口最大值.swift
+++ Project/239.滑动窗口最大值.swift
@@ -7,14 +7,28 @@
 // @lc code=start
 class Solution {
     func maxSlidingWindow(_ nums: [Int], _ k: Int) -> [Int] {
-        var maxInt = [Int]()
-        var maxRes = Int.min
-        var stack = nums[0..<k]
-        for val in nums[k..<nums.count] {
+            var maxIdx = [Int]()
+        var res = [Int]()
+        
+        for i in 0..<nums.count {
+            while maxIdx.count > 0 && nums[maxIdx.last!] < nums[i] {
+                maxIdx.removeLast()
+            }
             
+            maxIdx.append(i)
+            
+            if i >= k - 1 {
+                //主要是为了判断当前最开始那个大数的index是不是在当前窗口index范围
+           //也可以 maxIdx.first! + k <= i
+                if maxIdx.first! + k == i {
+                    maxIdx.removeFirst()//nums=[1,-1]，k=1会进来
+                }
+                
+                res.append(nums[maxIdx.first!])
+            }
         }
-
-        return maxInt
+        
+        return res
     }
 }
 // @lc code=end
